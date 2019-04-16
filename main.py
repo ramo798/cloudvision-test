ffd33923e3c1764b09d161017debca1d6668518d
ramo <knht31i@gmail.com>
2019-04-16 23:32:05 +0900

    送信用配列の作成

--- main.py
+++ main.py
@@ -5,9 +5,20 @@ import requests
 
 ENDPOINT_URL = 'https://vision.googleapis.com/v1/images:annotate'
 
+img_requests = [] #送信するファイル
+
 with open("receipt1.jpg", 'rb') as f:
-    ctxt = b64encode(f.read()).decode()
-    print(ctxt)
+    # ctxt = b64encode(f.read()) ＃デコードするとprintで表示された　要仕様確認
+    ctxt = b64encode(f.read()).decode() #base64エンコード
+    img_requests.append({
+        'image': {'content': ctxt},
+        'features': [{
+            'type': 'TEXT_DETECTION	',
+            'maxResults': 10
+        }]
+    })
+    # print(img_requests)
+    # print(ctxt)
     
 f.close()
 print("finish")
